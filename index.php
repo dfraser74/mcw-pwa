a17d20ff54aeeb3119e01b94379196dced882ca4
Yohan Totting <tyohan@gmail.com>
2017-12-13 16:30:31 +0700

    Update index with licence

--- index.php
+++ index.php
@@ -11,15 +11,23 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  wporg
 Domain Path:  /languages
 
-
-NOTE: 
+{Plugin Name} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+{Plugin Name} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with {Plugin Name}. If not, see {License URI}.
 */
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+
+
 defined( 'ABSPATH' ) or die( 'Nope, not accessing this' );
-// $rewrite=new WP_Rewrite;
-// print_r($rewrite->wp_rewrite_rules());die();
+
 require __DIR__ . '/vendor/autoload.php';
 require_once(dirname(__FILE__).'/WPWA.php');
 WPWA::instance();
